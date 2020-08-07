9e922330730cb43399bd4cc4edf1f9a2bffb90d7
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-07 13:04:25 +0530

    fix backticked commands with eval

--- setup-scripts/ubuntu.sh
+++ setup-scripts/ubuntu.sh
@@ -1,15 +1,15 @@
 echo "\n\nCLI-CONFIG: Updating apt\n\n"
 cmd='sudo apt update -y'
 echo "\n\nCLI-CONFIG: $cmd"
-`$cmd`
+eval $cmd
 
 echo "\n\nCLI-CONFIG: Setting up dependencies\n\n"
 cmd='sudo apt install python python-dev python-pip'
 echo "\n\nCLI-CONFIG: $cmd"
-`$cmd`
+eval $cmd
 
 packages='autojump thefuck'
 echo "\n\nCLI-CONFIG: Installing '$packages' \n\n"
 cmd="sudo apt install $packages"
 echo "\n\nCLI-CONFIG: $cmd"
-`$cmd`
+eval $cmd

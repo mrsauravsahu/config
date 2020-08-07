8b78b083ee55cddc14e5bc2bde6908412f9d3206
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-07 13:03:15 +0530

    display the commands being executed

--- setup-scripts/ubuntu.sh
+++ setup-scripts/ubuntu.sh
@@ -1,10 +1,15 @@
 echo "\n\nCLI-CONFIG: Updating apt\n\n"
-sudo apt update -y
+cmd='sudo apt update -y'
+echo "\n\nCLI-CONFIG: $cmd"
+`$cmd`
 
 echo "\n\nCLI-CONFIG: Setting up dependencies\n\n"
-deps='python python-dev python-pip'
-sudo apt install $deps
+cmd='sudo apt install python python-dev python-pip'
+echo "\n\nCLI-CONFIG: $cmd"
+`$cmd`
 
 packages='autojump thefuck'
 echo "\n\nCLI-CONFIG: Installing '$packages' \n\n"
-sudo apt install $packages
+cmd="sudo apt install $packages"
+echo "\n\nCLI-CONFIG: $cmd"
+`$cmd`

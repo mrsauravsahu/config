301270ace42074a3b5e88a84634a91be4a91c40c
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-07 12:54:18 +0530

    fix depedencies for thefuck on ubuntu

--- setup-scripts/ubuntu.sh
+++ setup-scripts/ubuntu.sh
@@ -1,5 +1,10 @@
-packages='autojump thefuck python python-dev pip'
+echo "\n\nCLI-CONFIG: Updating apt\n\n"
+sudo apt update -y
+
+echo "\n\nCLI-CONFIG: Setting up dependencies\n\n"
+deps='python python-dev python-pip'
+sudo apt install $deps -y
 
 echo "\n\nCLI-CONFIG: Installing '$packages' \n\n"
-sudo apt update -y
+packages='autojump thefuck'
 sudo apt install $packages -y

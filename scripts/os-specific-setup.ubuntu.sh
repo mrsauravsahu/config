2cd584dfbbbb666642135304859d28694668c6da
Saurav Sahu <mrsauravsahu@outlook.com>
2020-09-11 17:43:05 +0530

    remove sudo from commands

--- scripts/os-specific-setup.ubuntu.sh
+++ scripts/os-specific-setup.ubuntu.sh
@@ -1,15 +1,15 @@
 echo "\n\nCLI-CONFIG: Updating apt"
-cmd='sudo apt update -y'
+cmd='apt update -y'
 echo "CLI-CONFIG: $cmd"
 eval $cmd
 
 echo "\n\nCLI-CONFIG: Setting up dependencies"
-cmd='sudo apt install python python-dev python-pip'
+cmd='apt install python python-dev python-pip'
 echo "CLI-CONFIG: $cmd"
 eval $cmd
 
 packages='autojump thefuck'
 echo "\n\nCLI-CONFIG: Installing '$packages'"
-cmd="sudo apt install $packages"
+cmd="apt install $packages"
 echo "CLI-CONFIG: $cmd"
 eval $cmd

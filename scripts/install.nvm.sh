cb75e72cd041db03a61add9f3dca94b5edb36c8a
Saurav Sahu <mrsauravsahu@outlook.com>
2020-09-12 13:59:47 +0530

    setup nvm use

--- scripts/install.nvm.sh
+++ scripts/install.nvm.sh
@@ -12,3 +12,29 @@ nvm install --lts
 
 echo "\n\n# nvm configuration" >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'export NVM_DIR="$CLI_CONFIG_ROOT/current/nvm"' >> $CLI_CONFIG_PROGRAMS_CONF
+
+nvm_use_setup_string=$(cat <<EOF
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
+EOF
+)
+
+echo $nvm_use_setup_string >> $CLI_CONFIG_PROGRAMS_CONF
